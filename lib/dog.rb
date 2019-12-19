142e76ff0d5ed6650733a356df60192093d30d2f
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-19 04:16:07 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name = "Fido"
+  end
+	
+	def breed=(breed)
+	  @breed = breed
+	end
+	
+	def breed
+	  @breed = "Beagle"
+	 end
+end
+
